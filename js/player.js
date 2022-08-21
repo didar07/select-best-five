36e1c6cdd845b7e1b8d388dbdcff4cb88fa95554
didar07 <didarul111015@gmail.com>
2022-08-21 23:42:54 +0600

    player added

---
+++ js/player.js
@@ -0,0 +1,6 @@
+function addToCart(element) {
+    // console.log(element.parentNode.parentNode.children[0].innerText)
+
+    const playerName = element.parentNode.parentNode.children[0].innerText
+    console.log(playerName)
+}
