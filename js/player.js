3e117c4bed34a16aa0c07898938e6f5ba9c9cc2a
didar07 <didarul111015@gmail.com>
2022-08-22 12:53:06 +0600

    player selection added

--- js/player.js
+++ js/player.js
@@ -6,6 +6,9 @@ function display(totalPlayer) {
     tableBody.innerHTML = ''
 
     for (let i = 0; i < totalPlayer.length; i++) {
+        if (totalPlayer.length > 5) {
+            alert('opps you already selected 5 players')
+        }
         const name = playerCart[i].playerName
 
         const tr = document.createElement("tr")
@@ -26,4 +29,23 @@ function addToCart(element) {
     document.getElementById('total-added-player').innerText = playerCart.length
     display(playerCart)
 
+}
+
+function disableBtn() {
+    document.getElementById('btn-1').disabled = true;
+}
+function disableBtn2() {
+    document.getElementById('btn-2').disabled = true;
+}
+function disableBtn3() {
+    document.getElementById('btn-3').disabled = true;
+}
+function disableBtn4() {
+    document.getElementById('btn-4').disabled = true;
+}
+function disableBtn5() {
+    document.getElementById('btn-5').disabled = true;
+}
+function disableBtn6() {
+    document.getElementById('btn-6').disabled = true;
 }
