d82e889d015b11e86f8054269e382c462a2d6ab4
didar07 <didarul111015@gmail.com>
2022-08-22 10:37:33 +0600

    player call added

--- js/player.js
+++ js/player.js
@@ -1,6 +1,29 @@
-function addToCart(element) {
-    // console.log(element.parentNode.parentNode.children[0].innerText)
+const playerCart = []
 
+function display(totalPlayer) {
+
+    const tableBody = document.getElementById('call-player')
+    tableBody.innerHTML = ''
+
+    for (let i = 0; i < totalPlayer.length; i++) {
+        const name = playerCart[i].playerName
+
+        const tr = document.createElement("tr")
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}</td>
+        `
+        tableBody.appendChild(tr)
+    }
+}
+
+function addToCart(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText
-    console.log(playerName)
+
+    const playerObj = { playerName: playerName }
+    playerCart.push(playerObj)
+
+    document.getElementById('total-added-player').innerText = playerCart.length
+    display(playerCart)
+
 }
